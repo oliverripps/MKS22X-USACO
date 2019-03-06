c26695b5276fdb060f1167bc3320a5c1b9065751
Oliver Ripps <oripps@stuy.edu>
2019-03-06 10:42:10 -0500

    set up scanner and try block in bronze

--- USACO.java
+++ USACO.java
@@ -1,12 +1,23 @@
+import java.util.*;
+import java.io.*;
+
 public class USACO{
 
 
   public static int bronze(String filename){
+    try{
+      String str="";
+      File problem = new File(filename);
+      Scanner comp = new Scanner(problem);
+    }
+    else{
+      System.out.println("File Not Found:" + filename);
+    }
 
   }
 
   public static int silver(String filename){
-    
+
   }
 
 
