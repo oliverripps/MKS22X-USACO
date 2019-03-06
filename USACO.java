ce760365964c8ec38841f507ee58ec83bd5ff0ee
Oliver Ripps <oripps@stuy.edu>
2019-03-06 10:54:13 -0500

    added in taking input

--- USACO.java
+++ USACO.java
@@ -9,6 +9,12 @@ public class USACO{
       String str="";
       File problem = new File(filename);
       Scanner comp = new Scanner(problem);
+      str+=comp.nextLine();
+      List<String> eachnum = Arrays.asList(str.split(" "));
+      int r = eachnum.get(0);
+      int c = eachnum.get(1);
+      int e = eachnum.get(2);
+      int n = eachnum.get(3);
     }
     catch(FileNotFoundException e){
       System.out.println("File Not Found:" + filename);
