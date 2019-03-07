cd37929caa574c0a5e961d6c615208cfc0797faf
Oliver Ripps <oripps@stuy.edu>
2019-03-07 10:45:48 -0500

    added silver scanner and started silver problem

--- USACO.java
+++ USACO.java
@@ -11,10 +11,10 @@ public class USACO{
       Scanner comp = new Scanner(problem);
       str+=comp.nextLine();
       List<String> eachnum = Arrays.asList(str.split(" "));
-      int r = eachnum.get(0);
-      int c = eachnum.get(1);
-      int e = eachnum.get(2);
-      int n = eachnum.get(3);
+      int r = Integer.parseInt(eachnum.get(0));
+      int c = Integer.parseInt(eachnum.get(1));
+      int e = Integer.parseInt(eachnum.get(2));
+      int n = Integer.parseInt(eachnum.get(3));
     }
     catch(FileNotFoundException e){
       System.out.println("File Not Found:" + filename);
@@ -24,6 +24,36 @@ public class USACO{
   }
 
   public static int silver(String filename){
+    try{
+      String str="";
+      File problem = new File(filename);
+      Scanner comp = new Scanner(problem);
+      str+=comp.nextLine();
+      List<String> eachnum = Arrays.asList(str.split(" "));
+      int n = Integer.parseInt(eachnum.get(0));
+      int m = Integer.parseInt(eachnum.get(1));
+      int t = Integer.parseInt(eachnum.get(2));
+      int[][] nums= new int[n][m];
+      int[][] doc = new int[n][m];
+      for(int i =0;i<doc.length();i++){
+        str=comp.nextLine();
+        for(int l=0;l<doc[0].length();l++){
+          doc[i][l]=str.substring(l);
+        }
+      }
+      for(int i =0;i<doc.length();i++){
+        str=comp.nextLine();
+        for(int l=0;l<doc[0].length();l++){
+          doc[i][l]=str.substring(l);
+        }
+      }
+
+
+    }
+    catch(FileNotFoundException e){
+      System.out.println("File Not Found:" + filename);
+    }
+
     return 0;
   }
 
